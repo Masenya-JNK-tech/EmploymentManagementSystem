047d9adda9848c6cc09a4654f6a70d0710d7ba95
Keletso Masenya <165972501+Masenya-JNK-tech@users.noreply.github.com>
2026-07-17 11:40:10 +0200

    removed database credentials

--- src/database.py
+++ src/database.py
@@ -1,12 +1,13 @@
 import psycopg2
 
 def connect_database():
-#connect to the PostgreSQL database 
- connection = psycopg2.connect(
-  host= 'localhost',
-  user = 'postgres',
-  database =  'employee_management',
-  password = "M@senyakeletso05",
-  port = '2405'
- )
- return connection
+
+    connection = psycopg2.connect(
+        host="localhost",
+        database="database_name",
+        user="your_username",
+        password="password",
+        port="5432"
+    )
+
+    return connection
